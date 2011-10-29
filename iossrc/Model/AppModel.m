86c2c1aef7de99f5b68496daf594cf8062126bc9
Slobodan Pejic <slobo972@gmail.com>
2011-10-29 17:27:31 -0600

    Adding D10x10.

--- iossrc/Model/AppModel.m
+++ iossrc/Model/AppModel.m
@@ -35,6 +35,9 @@
 		     selected: NO];
 	[availableDice addDie: [SPDie dieWithSize: 10 andFacingSide: 10]
 		     selected: NO];
+	[availableDice addDie: [SPDie dieWithSize: 10 andFacingSide: 1
+				    andMultiplier: 10 andValueOffset: -10]
+		     selected: NO];
 	[availableDice addDie: [SPDie dieWithSize: 20 andFacingSide: 20]
 		     selected: NO];
 	return self;
